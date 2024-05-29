7218e4f6a38df0d81d949ed743a1c14170b53bdc
Loi <nguyenducloi1703@gmail.com>
2024-05-29 11:05:29 +0700

    Add csv browser, comparation

--- shiny/server.R
+++ shiny/server.R
@@ -1,59 +1,83 @@
-library(shiny)
-library(ggplot2)
-library(RColorBrewer)
-
 # Define server logic
-shinyServer(function(input, output, session) {
+server <- function(input, output, session) {
   
   dataset <- reactive({
-    switch(input$data,
-           "mtcars" = mtcars,
-           "iris" = iris)
+    req(input$file)
+    inFile <- input$file
+    read.csv(inFile$datapath)
   })
   
   observe({
     data <- dataset()
-    updateSelectInput(session, "xcol", choices = names(data), selected = names(data)[1])
-    updateSelectInput(session, "ycol", choices = names(data), selected = names(data)[2])
+    updateSelectInput(session, "xcol1", choices = names(data), selected = names(data)[1])
+    updateSelectInput(session, "ycol1", choices = names(data), selected = names(data)[2])
+    updateSelectInput(session, "xcol2", choices = names(data), selected = "")
+    updateSelectInput(session, "ycol2", choices = names(data), selected = "")
   })
   
-  output$plot <- renderPlot({
+ 
+   output$plot1 <- renderPlot({
     data <- dataset()
-    p <- ggplot(data, aes_string(x = input$xcol, y = input$ycol)) +
-      theme_minimal() + labs(x = input$xcol, y = input$ycol)
-    
-    # Determine the color to use based on the selected palette
-    color <- input$col
-    if (input$colorPalette == "protanopia") {
-      color <- brewer.pal(6, "Blues")[6]  # Color from Brewer palette that is better for Protanopia
-      p <- p + theme(
-        plot.title = element_text(color = brewer.pal(6, "Blues")[6]),
-        axis.title.x = element_text(color = brewer.pal(8, "Dark2")[2]),
-        axis.title.y = element_text(color = brewer.pal(8, "Dark2")[2]),
-        axis.text.x = element_text(color = brewer.pal(8, "Dark2")[2]),
-        axis.text.y = element_text(color = brewer.pal(8, "Dark2")[2]),
-        
-      )
-    }else if (input$colorPalette == "tritanopia") {
-      color <- brewer.pal(8, "Set1")[8]  # Color from Brewer palette that is better for Deuteranopia
-      p <- p + theme(
-        plot.title = element_text(color = brewer.pal(8, "Set1")[2]),
-        axis.title.x = element_text(color = brewer.pal(8, "Set1")[2]),
-        axis.title.y = element_text(color = brewer.pal(8, "Set1")[2]),
-        axis.text.x = element_text(color = brewer.pal(8, "Set1")[2]),
-        axis.text.y = element_text(color = brewer.pal(8, "Set1")[2])
-      )
+    if (input$removeNA) {
+      data <- na.omit(data)
     }
-    
-    # Add plot layers based on the selected plot type
-    if (input$plotType == "scatter") {
-      p <- p + geom_point(color = color)
-    } else if (input$plotType == "line") {
-      p <- p + geom_line(color = color)
-    } else if (input$plotType == "bar") {
-      p <- p + geom_bar(stat = "identity", fill = color)
-    }
-    
+    p <- create_plot(data, input$xcol1, input$ycol1)
     p
   })
-})
+  
+  
+  output$plot2 <- renderPlot({
+    data <- dataset()
+    if (input$removeNA) {
+      data <- na.omit(data)
+    }
+    p <- create_plot(data, input$xcol2, input$ycol2)
+    p
+  })
+  
+  output$table <- renderTable({
+    data <- dataset()
+    if (input$removeNA) {
+      data <- na.omit(data)
+    }
+    if (input$wantSecondChart && input$xcol2 != '' && input$ycol2 != '') {
+      diffs <- data.frame(X = data[, input$xcol1], Y1 = data[, input$ycol1], Y2 = data[, input$ycol2])
+      diffs$Difference <- diffs$Y1 - diffs$Y2
+      avg_diff <- diffs %>%
+        summarize(X = "Average",
+                  Y1 = mean(Y1, na.rm = TRUE),
+                  Y2 = mean(Y2, na.rm = TRUE),
+                  Difference = mean(Difference, na.rm = TRUE))
+      
+      # Combine the summary row with the original table
+      diffs <- rbind(diffs, avg_diff)
+      
+      diffs
+      diffs
+    } else {
+      NULL
+    }
+  })
+  
+  create_plot <- function(data, xcol, ycol) {
+    p <- ggplot(data, aes_string(x = xcol, y = ycol)) +
+      theme_minimal() +
+      labs(x = xcol, y = ycol)
+    
+    if (input$colorPalette == "protanopia") {
+      color <- brewer.pal(6, "Blues")[6]  # Color from Brewer palette that is better for Protanopia
+    } else if (input$colorPalette == "tritanopia") {
+      color <- brewer.pal(8, "Set1")[8]  # Color from Brewer palette that is better for Deuteranopia
+    }
+    
+    if (input$plotType == "scatter") {
+      p <- p + geom_point(color = color, size = input$pointSize)
+    } else if (input$plotType == "line") {
+      p <- p + geom_line(color = color, size = input$pointSize)
+    } else if (input$plotType == "bar") {
+      p <- p + geom_bar(stat = "identity", fill = color, width = (input$pointSize )/3)
+    }
+    
+    
+  }
+}
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,48 +1,63 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
-
 library(shiny)
 library(colourpicker)
 library(colourpicker)
 
 # Define UI for application that draws a histogram
 fluidPage(
-
-    # Application title
-    titlePanel("Plot customization app"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-          # sliderInput("bins",
-          #             "Number of bins:",
-          #             min = 1,
-          #             max = 50,
-          #             value = 30)
-          selectInput("data", "Choose a database",
-                      choices=list("mtcars", "iris"), selected = "mtcars"),
-          selectInput("xcol", "Variable X", c()),
-          selectInput("ycol", "Variable Y", c()),
-          colourInput("col", "Select color","black" ),
-          selectInput("plotType", "Choose Plot Type",
-                      choices = list("Scatter Plot" = "scatter", 
-                                     "Line Plot" = "line", 
-                                     "Bar Plot" = "bar")),
-          selectInput("colorPalette", "Choose Color Palette",
-                      choices = c("Default" = "default",
-                                  "Protanopia" = "protanopia",
-                                  "Tritanopia" = "tritanopia"))
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-          plotOutput("plot")
-        )
+  
+  # Application title
+  titlePanel("Plot customization app"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    
+    sidebarPanel(
+      width = 3,
+      fileInput("file", "Choose CSV File", accept = c(".csv")),
+      uiOutput("xvar_ui"),
+      uiOutput("yvar_ui"),
+      #selectInput("data", "Choose a database",
+      #            choices=list("mtcars", "iris"), selected = "mtcars"),
+      selectInput("xcol1", "X-Axis variable (Plot 1)", ""),
+      selectInput("ycol1", "Y-Axis variable (Plot 1)", ""),
+      checkboxInput("wantSecondChart", "Want to compare?", value = FALSE),
+      conditionalPanel(
+        condition = "input.wantSecondChart",
+        selectInput("xcol2", "X-Axis variable (Plot 2)", ""),
+        selectInput("ycol2", "Y-Axis variable (Plot 2)", ""),
+        checkboxInput("wantShowTable", "Want to show differences?", value = FALSE)
+      ),
+      colourInput("col", "Select color","black" ),
+      selectInput("plotType", "Choose Plot Type",
+                  choices = list("Scatter Plot" = "scatter", 
+                                 "Line Plot" = "line", 
+                                 "Bar Plot" = "bar")),
+      selectInput("colorPalette", "Choose Color Palette",
+                  choices = c("Default" = "default",
+                              "Protanopia" = "protanopia",
+                              "Tritanopia" = "tritanopia")),
+      checkboxInput("removeNA", "Remove NA values", value = TRUE),
+      sliderInput("pointSize", "Point Size", min = 1, max = 5, value = 3)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      fluidRow(
+        column(width = 6, plotOutput("plot1")),
+        column(width = 6, 
+               conditionalPanel(
+                 condition = "input.xcol2 != '' && input.ycol2 != ''",
+                 plotOutput("plot2")
+               )
+      ),
+      column(width = 12, align = "center",
+             conditionalPanel(
+               condition = "input.wantShowTable",
+               tableOutput("table")
+             )
+      )
+      
+      )
     )
+  )
 )
