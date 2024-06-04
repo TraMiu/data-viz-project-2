b033b7247423766c5e41fb2c88b06ed7d1922ba3
Loi <nguyenducloi1703@gmail.com>
2024-06-04 21:45:33 +0700

    Finalize product

--- shiny/server.R
+++ shiny/server.R
@@ -2,53 +2,155 @@ library(shiny)
 library(colourpicker)
 library(tidyr)
 library(ggplot2)
-library(dplyr)
+library(dplyr) 
 library(magrittr)
 library(RColorBrewer)
-library(htmlwidgets)
-library(webshot)
-library(plotly)
+library(showtext)
+library(colorblindr)
+
+
+font_add_google("Montserrat", "Montserrat")
+main_font = "Montserrat"
+
+
+vinuni_palette_main <- c("#35426e", "#d2ae6d", "#c83538", "#2e548a")
+vinuni_palette_accents <- c( "#5cc6d0", "#a7c4d2", "#d2d3d5",  "#4890bd", "#0087c3", "#d2ae6d")
+
+# Saving our theme as a function
+theme_vinuni <- function(base_size = 11, base_family = main_font, 
+                         base_line_size = base_size / 22, 
+                         base_rect_size = base_size / 22) {
+  # Base our theme on minimal theme
+  theme_minimal(
+    base_family = base_family,
+    base_size = base_size,
+    base_line_size = base_line_size,
+    base_rect_size = base_rect_size
+  ) +
+    theme(
+      plot.title.position = "plot",
+      plot.title = element_text(hjust = 0.5, face="bold", colour = vinuni_palette_main[3]),
+      plot.subtitle = element_text(hjust = 0.5, face="bold"),
+      legend.position = "bottom",
+      panel.grid.major.x = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.y = element_line(color = "#bbbbbb", linewidth = 0.2),
+      panel.grid.minor.y = element_blank(),
+      axis.text = element_text(size = rel(1.0)),
+      axis.text.x = element_text(face="bold", colour = vinuni_palette_main[1]), 
+      axis.text.y = element_text(face="bold", colour = vinuni_palette_main[1]), 
+      legend.text = element_text(size = rel(0.9))
+    )
+}
+
 
 # Define server logic
 server <- function(input, output, session) {
   
+  # Input csv
   dataset <- reactive({
     req(input$file)
     inFile <- input$file
     read.csv(inFile$datapath)
   })
   
-  observe({
-    data <- dataset()
-    cols <- names(data)
-    updateSelectInput(session, "xcol1", choices = cols, selected = cols[1])
-    updateSelectInput(session, "ycol1", choices = cols, selected = cols[2])
-    updateSelectInput(session, "xcol2", choices = cols, selected = "")
-    updateSelectInput(session, "ycol2", choices = cols, selected = "")
+  # Generate UI for selecting the fill variable
+  output$fillcol_ui <- renderUI({
+    req(dataset())
+    selectInput("fillcol", "Fill Variable", choices = names(dataset()))
   })
   
-  get_filtered_data <- reactive({
+  # Generate UI for selecting the color variable
+  output$colorcol_ui <- renderUI({
+    req(dataset())
+    selectInput("colorcol", "Color Variable", choices = names(dataset()))
+  })
+  
+  
+  
+  # Populate the choice for each selection option
+  observe({
+    data <- dataset()
+    updateSelectInput(session, "xcol1", choices = names(data), selected = names(data)[1])
+    updateSelectInput(session, "ycol1", choices = names(data), selected = names(data)[2])
+    updateSelectInput(session, "xcol2", choices = names(data), selected = "")
+    updateSelectInput(session, "ycol2", choices = names(data), selected = "")
+  })
+  
+  output$nonFrequentGuidanceText <- renderText({"Please go to Plot Customization to select another plot type."})
+  
+  # render plot 1
+  output$plot1 <- renderPlot({
     data <- dataset()
     if (input$removeNA) {
       data <- na.omit(data)
     }
-    data
+    
+    if (input$changeLabel1) {
+      xlabel <- input$plotX_1label
+    } else {
+      xlabel <- input$xcol1
+    }
+    
+    if (input$changeLabel1) {
+      ylabel <- input$plotY_1label
+    } else {
+      ylabel <- input$ycol1
+    }
+    
+    p <- create_plot(data, input$xcol1, input$ycol1, xlabel, ylabel)
+    p
   })
   
-  output$plot1 <- renderPlot({
-    data <- get_filtered_data()
-    create_plot(data, input$xcol1, input$ycol1, input$colorPalette, input$plotType, input$pointSize)
+  # Render UI for plot based on checkbox input
+  output$dynamicPlot <- renderUI({
+    if (input$colorBlind) {
+      plotOutput("plot1")
+    } else {
+      fluidRow(
+        column(width = 6, plotOutput("plot1")),
+        column(width = 6, 
+               conditionalPanel(
+                 condition = "input.xcol2 != '' && input.ycol2 != ''",
+                 plotOutput("plot2")
+               )
+        )
+      )
+    }
   })
   
+  # render plot 2
   output$plot2 <- renderPlot({
-    data <- get_filtered_data()
-    create_plot(data, input$xcol2, input$ycol2, input$colorPalette, input$plotType, input$pointSize)
+    data <- dataset()
+    if (input$removeNA) {
+      data <- na.omit(data)
+    }
+    
+    if (input$changeLabel2) {
+      xlabel <- input$plotX_1label
+    } else {
+      xlabel <- input$xcol1
+    }
+    
+    if (input$changeLabel2) {
+      ylabel <- input$plotY_2label
+    } else {
+      ylabel <- input$ycol2
+    }
+    
+    p <- create_plot(data, input$xcol2, input$ycol2, xlabel, ylabel)
+    p
+    
   })
   
+  # Draw table for different
   output$table <- renderTable({
-    data <- get_filtered_data()
+    data <- dataset()
+    if (input$removeNA) {
+      data <- na.omit(data)
+    }
     if (input$wantSecondChart && input$xcol2 != '' && input$ycol2 != '') {
-      diffs <- data.frame(X = data[[input$xcol1]], Y1 = data[[input$ycol1]], Y2 = data[[input$ycol2]])
+      diffs <- data.frame(X = data[, input$xcol1], Y1 = data[, input$ycol1], Y2 = data[, input$ycol2])
       diffs$Difference <- diffs$Y1 - diffs$Y2
       avg_diff <- diffs %>%
         summarize(X = "Average",
@@ -56,33 +158,81 @@ server <- function(input, output, session) {
                   Y2 = mean(Y2, na.rm = TRUE),
                   Difference = mean(Difference, na.rm = TRUE))
       
+      # Combine the summary row with the original table
       diffs <- rbind(diffs, avg_diff)
+      
+      diffs
       diffs
     } else {
       NULL
     }
   })
   
-  create_plot <- function(data, xcol, ycol, palette, plotType, pointSize) {
-    if (xcol == '' || ycol == '') return(NULL)
+  create_plot <- function(data, xcol, ycol, xlabel, ylabel) {
     
-    p <- ggplot(data, aes_string(x = xcol, y = ycol)) +
-      theme_minimal() +
-      labs(x = xcol, y = ycol)
+    if (input$plotType == "scatter") {
+      g <- ggplot(data, aes_string(x = xcol, y = ycol))
+    } else if (input$plotType == "line") {
+      g <- ggplot(data, aes_string(x = xcol, y = ycol))
+      req(input$colorcol)
+      g <- ggplot(data, aes_string(x = xcol, y = ycol, color = input$colorcol))
+    } else if (input$plotType == "bar") {
+      g <- ggplot(data, aes_string(x = xcol, y = ycol))
+      req(input$fillcol)
+      g <- ggplot(data, aes_string(x = xcol, y = ycol, fill = input$fillcol))
+    }
     
-    color <- switch(palette,
-                    "protanopia" = brewer.pal(6, "Blues")[6],
-                    "tritanopia" = brewer.pal(8, "Set1")[8],
-                    "black")
     
-    p <- p + switch(plotType,
-                    "scatter" = geom_point(color = color, size = pointSize),
-                    "line" = geom_line(color = color, size = pointSize, group = 1),
-                    "bar" = geom_bar(stat = "identity", fill = color, width = pointSize / 3))
+    
+    
+    if (input$colorPalette == "protanopia") {
+      color <- brewer.pal(6, "Blues")[6]  # Color from Brewer palette that is better for Protanopia
+    } else if (input$colorPalette == "tritanopia") {
+      color <- brewer.pal(8, "Set1")[8]  # Color from Brewer palette that is better for Deuteranopia
+    } else {color <- "black"}
+    
+    
+    
+    if (input$plotType == "scatter") {
+      p <- g + geom_point(color = color, size = input$pointSize)
+    } else if (input$plotType == "line") {
+      #p <- g + geom_line(color = color, size = input$pointSize, group = 1)
+      req(input$colorcol) 
+      p <- g + geom_line(color = color, size = input$pointSize, group = 1)
+    } else if (input$plotType == "bar") {
+      #p <- g + geom_bar(stat = "identity", fill = color, width = (input$pointSize )/3)
+      req(input$fillcol) 
+      p <- g + geom_col(stat = "identity", position = "dodge", width = (input$pointSize )/3)
+    } 
     
     p
+    
+    # Apply colorblind adjustments if checked
+    if (input$colorBlind) {
+      p <- cvd_grid(p)
+    }
+    
+    # Apply selected theme
+    p <- switch(input$themeInput,
+                "minimal" = p + theme_minimal(),
+                "dark" = p + theme_dark(),
+                "light" = p + theme_light(),
+                "grey" = p + theme_grey(),
+                "classic" = p + theme_classic(),
+                "vinuni" = p + theme_vinuni(),
+                "void" = p + theme_void()
+    )
+    
+    # Return the plot
+    p + labs(
+      x = xlabel, 
+      y = ylabel,
+      title = ifelse(input$plotTitle == "", input$file, input$plotTitle),
+      subtitle = input$plotSubtitle)
   }
   
+  
+  # This is for the real time data
   RealTime <- function() {
     read_data <- reactiveFileReader(input$refreshRate * 1000, session, filePath = "data.csv", readFunc = read.csv)
     
@@ -92,7 +242,7 @@ server <- function(input, output, session) {
       data <- data[data$Date >= input$dateRange[1] & data$Date <= input$dateRange[2], ]
       data
     })
-
+    
     
     output$plot3 <- renderPlot({
       
@@ -101,18 +251,18 @@ server <- function(input, output, session) {
         color <- brewer.pal(6, "Blues")[6]  
       } else if (input$colorPalette1 == "tritanopia") {
         color <- brewer.pal(8, "Set1")[8] 
-      } else {"black"}
-      plot_type <- switch(input$plotType1,
-                            "scatter" = geom_point(color = color),
-                            "line" = geom_line(color = color),
-                            "bar" = geom_boxplot(color = color))
-         ggplot(data, aes(x = Date, y = X1,  group = 1)) +
-           stat_summary(fun.y = "mean") +
+      } else {color <- "black"}
+      plot_type <- switch(input$plotType2,
+                          "scatter" = geom_point(color = color),
+                          "line" = geom_line(color = color, size = pointSize, group = 1),
+                          "bar" = geom_boxplot(color = color))
+      ggplot(data, aes(x = Date, y = X1,  group = 1)) +
+        stat_summary(fun.y = "mean") +
         theme(axis.text.x = element_text(angle = 45, hjust = 1))+
         plot_type +
         labs(x = "Date", y = "Price") + 
         ggtitle("Stock Price day by day")
-        })
+    })
     
     
     
@@ -129,14 +279,14 @@ server <- function(input, output, session) {
         color <- brewer.pal(6, "Blues")[6]  
       } else if (input$colorPalette1 == "tritanopia") {
         color <- brewer.pal(8, "Set1")[8] 
-      } else {"black"}
+      } else {color <- "black"}
       
       ggplot(data, aes(x = Date, y = X1)) +
         geom_boxplot(color=color) +
         labs(x = "Date", y = "Price") +
         ggtitle("Stock Price Over Time")})
     
-
+    
     
     searchedData <- reactive({
       data <- read_data()
@@ -175,7 +325,7 @@ server <- function(input, output, session) {
     notification <- paste("Snapshot saved as", filename, "in the current working directory.")
     showNotification(notification, duration = 5)
   })
-
+  
   
   observeEvent(input$choice, {
     if (input$choice == "Real Time Data") {
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,111 +1,203 @@
 library(shiny)
-library(shinythemes)
 library(colourpicker)
 library(tidyr)
 library(ggplot2)
-library(dplyr)
+library(dplyr) 
 library(magrittr)
 library(RColorBrewer)
 
 # Define UI for application that draws a histogram
 fluidPage(
   
-  # Use a theme from shinythemes
-  theme = shinytheme("flatly"),
-  
   # Application title
-  titlePanel("Group E Project"),
+  titlePanel("Shiny plot customization app"),
   
-  # Dropdown to choose between static and real-time data
-  selectInput("choice", "Choose Option", choices = c("Static Data", "Real Time Data"), width = 190),
+  tabsetPanel(
+    id = "tabset",
+    tabPanel("Data Preparation"),
+    tabPanel("Parameter Selection"),
+    tabPanel("Plot Customization"),
+    tabPanel("Theme & Accessibility")
+  ),
   
-  # Sidebar layout with input controls
+  selectInput("choice", "Data Type", choices = c("Static Data", "Real Time Data"), width = 190),
+  textInput("plotTitle", "Enter Plot Title", value = "", width = 190),
+  checkboxInput("wantPlotSubtitle", "Add subtitle?", value = FALSE),
+  
+  conditionalPanel(
+    condition = "input.wantPlotSubtitle",
+    textInput("plotSubtitle", "Enter Plot Subtitle", value = "", width = 190),
+  ),
+  
   sidebarLayout(
     sidebarPanel(
       width = 3,
+      
+      # Data preparation tab
       conditionalPanel(
-        condition = "input.choice == 'Static Data'",
-        fileInput("file", "Choose CSV File", accept = c(".csv")),
-        uiOutput("xvar_ui"),
-        uiOutput("yvar_ui"),
-        selectInput("xcol1", "X-Axis Variable (Plot 1)", ""),
-        selectInput("ycol1", "Y-Axis Variable (Plot 1)", ""),
-        checkboxInput("wantSecondChart", "Want to Compare?", value = FALSE),
+        condition = "input.tabset == 'Data Preparation' && input.choice == 'Static Data'",
+        checkboxInput("removeNA", "Remove NA values", value = TRUE),
+        fileInput("file", "Import CSV File", accept = c(".csv")),
+      ),
+      
+      
+      # Parameter selection tab
+      conditionalPanel(
+        condition = "input.tabset == 'Parameter Selection' && input.choice == 'Static Data'",
+        selectInput("xcol1", "X-Axis variable (Plot 1)", ""),
+        selectInput("ycol1", "Y-Axis variable (Plot 1)", ""),
+        
+        
+        checkboxInput("changeLabel1", "Change labels of plot 1?", value = FALSE),
+        conditionalPanel(
+          condition = "input.changeLabel1",
+          textInput("plotX_1label", "Enter X label", value = ""),
+          textInput("plotY_1label", "Enter Y label", value = ""),
+        ),
+        
+        checkboxInput("wantSecondChart", "Want to compare?", value = FALSE),
         conditionalPanel(
           condition = "input.wantSecondChart",
-          selectInput("xcol2", "X-Axis Variable (Plot 2)", ""),
-          selectInput("ycol2", "Y-Axis Variable (Plot 2)", ""),
-          checkboxInput("wantShowTable", "Want to Show Differences?", value = FALSE)
+          selectInput("xcol2", "X-Axis variable (Plot 2)", ""),
+          selectInput("ycol2", "Y-Axis variable (Plot 2)", ""),
+          
+          checkboxInput("changeLabel2", "Change labels of plot 2?", value = FALSE),
+          conditionalPanel(
+            condition = "input.changeLabel2",
+            textInput("plotX_2label", "Enter X label", value = ""),
+            textInput("plotY_2label", "Enter Y label", value = ""),
+          ),
+          checkboxInput("wantShowTable", "Want to show differences?", value = FALSE)
         ),
-        colourInput("col", "Select Color", "black"),
+        
+      ),
+      
+      
+      
+      # Select Plot Type
+      conditionalPanel(
+        condition = "input.tabset == 'Plot Customization' && input.choice == 'Static Data'",
         selectInput("plotType", "Choose Plot Type",
                     choices = list("Scatter Plot" = "scatter", 
                                    "Line Plot" = "line", 
                                    "Bar Plot" = "bar")),
+        
+        conditionalPanel(
+          condition = "input.plotType == 'bar'",
+          checkboxInput("categorizeBar", "Want to categorize bars?", value = FALSE),
+          conditionalPanel(
+            condition = "input.categorizeBar",
+            uiOutput("fillcol_ui")
+          )
+        ),
+        
+        conditionalPanel(
+          condition = "input.plotType == 'line'",
+          checkboxInput("categorizeLine", "Want to categorize lines?", value = FALSE),
+          conditionalPanel(
+            condition = "input.categorizeLine",
+            uiOutput("colorcol_ui")
+          )
+        ),
+        sliderInput("pointSize", "Point Size", min = 1, max = 5, value = 3),
+      ),
+      
+      
+      
+      
+      # Select theme
+      conditionalPanel(
+        condition = "input.tabset == 'Theme & Accessibility' && input.choice == 'Static Data'",
+        colourInput("col", "Select color","black" ),
         selectInput("colorPalette", "Choose Color Palette",
                     choices = c("Default" = "default",
                                 "Protanopia" = "protanopia",
-                                "Tritanopia" = "tritanopia")),
-        checkboxInput("removeNA", "Remove NA Values", value = TRUE),
-        sliderInput("pointSize", "Point Size", min = 1, max = 5, value = 3)
+                                "Tritanopia" = "tritanopia"), selected = "protanopia"),
+        selectInput("themeInput", "Select a Theme", 
+                    choices = c(
+                      "Minimal" = "minimal", 
+                      "Dark" = "dark", 
+                      "Light" = "light",
+                      "Grey" = "grey",
+                      "Classic" = "classic", 
+                      "Void" = "void",
+                      "VinUniversity Theme*" = "vinuni")),
+        checkboxInput("colorBlind", "Color blind friendly", value = FALSE)
       ),
+      
+      
+      # UI for real time data
       conditionalPanel(
         condition = "input.choice == 'Real Time Data'",
-        selectInput("plotType1", "Choose Plot Type",
+        
+        
+        conditionalPanel(
+          condition = "input.tabset == 'Plot Customization' && input.notFrequencyChart",
+          selectInput("plotType1", "Choose Plot Type",
+                      choices = list("Scatter Plot" = "scatter", 
+                                     "Line Plot" = "line", 
+                                     "Bar Plot" = "bar"))
+        ),
+        conditionalPanel(
+          condition = "input.tabset == 'Theme & Accessibility'",
+          selectInput("colorPalette1", "Choose Color Palette",
+                      choices = c("Default" = "default",
+                                  "Protanopia" = "protanopia",
+                                  "Tritanopia" = "tritanopia"))
+        ),
+        selectInput("plotType2", "Choose Plot Type",
                     choices = list("Scatter Plot" = "scatter", 
                                    "Line Plot" = "line", 
                                    "Box Plot" = "bar")),
-        checkboxInput("wantShowSummary", "Show Data", value = FALSE),
-        selectInput("colorPalette1", "Choose Color Palette",
-                    choices = c("Default" = "default",
-                                "Protanopia" = "protanopia",
-                                "Tritanopia" = "tritanopia")),
-        sliderInput("refreshRate", "Refresh After (seconds):", min = 1, max = 5, value = 3),
         textInput("searchTerm", "Search Data"),
+        checkboxInput("wantShowSummary", "Show Data", value = FALSE),
         dateRangeInput("dateRange", "Select Date Range", start = Sys.Date() - 30, end = Sys.Date()),
+        
+        sliderInput("refreshRate", "Refresh After (second): ", min = 5, max = 10, value = 7),
         actionButton("snapshot_btn", "Take Snapshot", class = "btn-primary"),
         
       )
     ),
     
-    # Main panel for displaying outputs
+    # Show a plot of the generated distribution
     mainPanel(
+      
+      # For static data without color blind and with second plot
       conditionalPanel(
         condition = "input.choice != 'Real Time Data'",
-        fluidRow(
-          column(width = 6, plotOutput("plot1")),
-          column(width = 6, 
-                 conditionalPanel(
-                   condition = "input.xcol2 != '' && input.ycol2 != ''",
-                   plotOutput("plot2")
-                 )
-          )
-        ),
+        uiOutput("dynamicPlot"),
         fluidRow(
           column(width = 12, align = "center",
                  conditionalPanel(
                    condition = "input.wantShowTable",
-                   tableOutput("table"),
-                   
+                   tableOutput("table")
                  )
           )
         )
       ),
-      conditionalPanel(
-        condition = "input.choice == 'Real Time Data'",
-        column(width = 6,plotOutput("plot3")),
-        column(width = 6,plotOutput("plot4")),
-        fluidRow(
-          column(width = 12, align = "center",
-                 conditionalPanel(
-                   condition = "input.wantShowSummary",
-                   tableOutput("table1"),
-
-                   dataTableOutput("searchResultsTable")
-                 )
+      
+      
+      # For Real Time Data
+      fluidRow(
+        column(
+          width = 12,
+          conditionalPanel(
+            condition = "input.choice == 'Real Time Data'",
+            column(width = 8,plotOutput("plot3")),
+            column(width = 4,plotOutput("plot4")),
+            fluidRow(
+              column(width = 12, align = "center",
+                     conditionalPanel(
+                       condition = "input.wantShowSummary",
+                       tableOutput("table1"),
+                       
+                       dataTableOutput("searchResultsTable")
+                     )
+              )
+            )
+            )
           )
         )
       )
     )
   )
-)
